fed2ff370d70424952f6e9055f143ba85c128851
David Sabau <127231858+davosab@users.noreply.github.com>
2023-05-10 17:43:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,31 @@ const
   //Screen selectors
   previousEquation = document.getElementById("previous-equation"),
   result = document.getElementById("result");
-
+  
 //Functions
 function removeLast() {
   result.innerHTML = result.innerHTML.slice(0, -1);
 }
+
 function allClear() {
   result.innerHTML = "";
   previousEquation.innerHTML = "";
 }
+
 function solveEquation() {
   previousEquation.innerHTML =
     result.innerHTML.replace(/\(|\)|\÷|\×|\-|\+/g, x => `<span>${x}</span>`);
   result.innerHTML = eval(result.innerHTML.replace("×", "*").replace("÷", "/")
-    .replace(/[a-z]/ig, "")); // Make eval() safe
-}
+    .replace(/[a-z]/ig, "")).toFixed(10);// Make eval() safe
+  while (result.innerHTML.endsWith(0) || result.innerHTML.endsWith("."))
+    removeLast();
+  };
 
 //Events
 for (let i = 0; i < operands.length; i++) {
   operands[i].onclick = () => {
-    if (result.innerHTML.length == 11) {
-      alert("Hopa! \nEcuația poate fi de cel mult 11 cifre.");
+    if (result.innerHTML.length == 22) {
+      alert("Hopa! \nEcuația poate fi de cel mult 22 cifre.");
       return; 
     }
     result.innerHTML += operands[i].innerHTML;
